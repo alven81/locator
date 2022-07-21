cf6e8712fe8277b17f6a2e6a5564c6c5b540b19e
Aliaksandr Kot <alven81.ak@gmail.com>
2022-07-21 16:04:26 +0200

    fix

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,24 +1,14 @@
-const List = ({listData}) => {
-    console.log(listData.length);
+const List = ({infoList}) => {
+    console.log(infoList.length);
     return (
         <div className="container main_list">
-            {
-                listData.length ? <p>Loading</p> : 
-                listData.map((item) => (
-                    
+             {
+                infoList.length > 0 ? infoList.map((item) => (   
                     <p>{item.ip}</p>
-                    
-                ))
-                    
+                )) :  <p>Loading</p> 
             }
-
-
         </div>
     );
 };
 
 export { List };
-
-// {consist.map((value, id) => (
-//     <li key={id}>{value}</li>
-// ))}
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,40 +1,42 @@
 import { List } from "./List";
 import { User } from "./User";
 import { Location } from "./Location";
+import { data } from "../utils/temp";
 import { useState } from "react";
 import axios from "axios";
 
 const Main = () => {
-
     const [listData, setListData] = useState([]);
     const [inputText, setInputText] = useState("");
-    const access_key = "9c55f3883b50ad85b49de542782e451d";
+    const access_key = "44894fdb87a8efa4d68d6a6959c11f07";
 
     const request = (inputText) => {
-        axios.get(`http://api.ipstack.com/${inputText}?access_key=${access_key}`, {
-          })
-          .then(function (response) {
-            builtListData(response.data)
-          })
-          .then (
-            console.log(listData)
-          )
-          .catch(function (error) {
-            console.log(error);
-          })
-    }
+        axios
+            .get(
+                `http://api.ipstack.com/${inputText}?access_key=${access_key}`,
+                {}
+            )
+            .then(function (response) {
+                builtListData(response.data);
+            })
+            .then(console.log(listData))
+            .catch(function (error) {
+                console.log(error);
+            });
+    };
 
     const builtListData = (input) => {
         //console.log(input)
         const tempListData = listData;
-       tempListData.push(input);
-       setListData(tempListData)
-    }
+        tempListData.push(input);
+        setListData(tempListData);
+        console.log(listData);
+    };
 
     return (
         <div className="main_locator">
             <div className="main_locator-list">
-                {listData && <List listData={listData}/>}
+           { listData && <List infoList={listData} />}
             </div>
             <div className="main_locator-info">
                 <div className="main_locator-info-location">
@@ -46,8 +48,9 @@ const Main = () => {
                     </div>
                 </div>
                 <div className="main_locator-info-search">
-                    <input onChange={e => setInputText(e.target.value)} />
-                    <button onClick={() => request(inputText)}>search</button>
+                    <input onChange={(e) => setInputText(e.target.value)} />
+                    <button onClick={() => builtListData(data)}>search</button>
+                    {/* <button onClick={() => request(inputText)}>search</button>  */}
                 </div>
                 <div className="main_locator-info-location">
                     <div>
